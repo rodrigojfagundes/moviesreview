22b4d06bdf07ccb2cfb18e610b2b9e3b2e76279e
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-09-07 18:58:21 -0300

    UserDTO, RoleDTO created

---
+++ backend/moviesreview/src/main/java/io/github/rodrigojfagundes/moviesreview/dto/UserDTO.java
@@ -0,0 +1,81 @@
+package io.github.rodrigojfagundes.moviesreview.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import io.github.rodrigojfagundes.moviesreview.entities.User;
+
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	@NotBlank(message = "Campo obrigatorio")
+	private String name;
+	
+	@Email(message = "Favor digitar um email valido")
+	private String email;
+	
+	Set<RoleDTO> roles = new HashSet<>();
+	
+	
+	public UserDTO() {}
+
+	
+	public UserDTO(Long id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+	public UserDTO(User entity) {
+		id = entity.getId();
+		name = entity.getName();
+		email = entity.getEmail();
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+	
+	
+	
+}
---
+++ backend/moviesreview/src/main/java/io/github/rodrigojfagundes/moviesreview/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package io.github.rodrigojfagundes.moviesreview.dto;
+
+//verifica se o email q eu estou inserindo ja exist n banco
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
